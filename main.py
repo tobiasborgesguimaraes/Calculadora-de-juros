b2a7709788be049e827c19e769baa8a8974058a2
Tobias Borges <tobias.borgesguimaraes@gmail.com>
2024-01-30 16:04:17 -0300

    Entries e Labels adicionadas

--- main.py
+++ main.py
@@ -5,7 +5,31 @@ root = tk.Tk()
 class Aplicação:
     def __init__(self):
         self.root = root
+        self.tela()
+        self.widgets()
         self.root.mainloop()
+    
+    def tela(self):
+        self.root.title('Calculadora de juros')
+        self.root.configure(background= "#FFE8CD")
+        self.root.geometry('700x500')
+        self.root.resizable(False, False)
+    
+    def widgets(self):
+        self.labelCapital = tk.Label(root, text= 'Capital', bg= "#FFE8CD")
+        self.labelCapital.place(relx= 0.2, rely= 0.1, relwidth= 0.1, relheight=0.05)
+        self.entryCapital = tk.Entry(root)
+        self.entryCapital.place(relx= 0.15, rely= 0.2, relwidth= 0.2, relheight=0.05)
+
+        self.labelTempo = tk.Label(root, text= 'Tempo', bg= "#FFE8CD")
+        self.labelTempo.place(relx= 0.45, rely= 0.1, relwidth= 0.1, relheight=0.05)
+        self.entryTempo = tk.Entry(root)
+        self.entryTempo.place(relx= 0.4, rely= 0.2, relwidth= 0.2, relheight=0.05)
+
+        self.labelJuros = tk.Label(root, text= 'Juros', bg= "#FFE8CD")
+        self.labelJuros.place(relx= 0.7, rely= 0.1, relwidth= 0.1, relheight=0.05)
+        self.entryJuros = tk.Entry(root)
+        self.entryJuros.place(relx= 0.65, rely= 0.2, relwidth= 0.2, relheight=0.05)
 
 
 Aplicação()
